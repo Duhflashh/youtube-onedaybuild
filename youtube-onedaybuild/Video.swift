a7692d28c07002aa2af1c014d8f1255316198c38
Duhflashh <73899377+Duhflashh@users.noreply.github.com>
2021-11-02 00:01:50 -0400

    added code to parse the json response

--- youtube-onedaybuild/Video.swift
+++ youtube-onedaybuild/Video.swift
@@ -7,12 +7,57 @@
 
 import Foundation
 
-struct Video {
+struct Video : Decodable {
     
     var videoId = ""
     var title = ""
     var description = ""
     var thumbnail = ""
-    var published = "" 
+    var published = Date()
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case thumbnails
+        case snippet
+        case high
+        case resourceId
+        
+        case published = "publishedAt"
+        case title
+        case description
+        case thumbnail = "url"
+        case videoId
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container =   try decoder.container(keyedBy: CodingKeys.self)
+        
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+        
+        // Parse title
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        
+        // Parse description
+        self.description = try snippetContainer.decode(String.self, forKey: .description)
+        
+        // Parse the publish date
+        self.published = try snippetContainer.decode(Date.self, forKey: .published)
+        
+        // Parse thumbnails
+        let thumbnailsContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+        
+        
+        let highContainer = try thumbnailsContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+        
+        // Parse URL
+        self.thumbnail = try highContainer.decode(String.self, forKey: .thumbnail)
+        
+        let resourceIdContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        
+        self.videoId = try resourceIdContainer.decode(String.self, forKey: .videoId)
+        
+        
+    }
     
 }
