e6e2418ec1c15bacc508b624455237138700f33d
Duhflashh <73899377+Duhflashh@users.noreply.github.com>
2021-11-01 22:10:49 -0400

    modified gitignore to stop tracking constants.swift, set up api to parse the json

---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Michael Duberry on 11/1/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = "" 
+    
+}
