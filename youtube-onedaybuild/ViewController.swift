54b0e8d88c3da87037d0cc7514dc0a6645f20a05
Duhflashh <73899377+Duhflashh@users.noreply.github.com>
2021-11-09 19:24:50 -0500

    added tableview, custom protocol in the model, displaying video titles in tableview

--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -9,17 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var model = Model()
+    @IBOutlet weak var tableView: UITableView!
     
+    
+    
+    var model = Model()
+    var videos = [Video]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Set itself as the datasource and the delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Set itself as the delegate of the model
+        model.delegate = self
         
         model.getVideos()
     
     }
 
 
+}
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource, ModelDelegate {
+    
+    func videosFetched(_ videos: [Video]) {
+        
+        // Set the returned videos to our video property
+        self.videos = videos
+        
+        // Refresh the tableview
+        tableView.reloadData()
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        // Configure the cell with the data
+        // Get the title for the video in question
+        let title = self.videos[indexPath.row].title
+        
+        cell.textLabel?.text = title
+        
+        // Return the cell to display
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+   
+    
+    
+    
 }
 
